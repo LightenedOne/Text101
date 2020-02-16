6e31944da4dcc235cdd0aee996323587c3dd52d7
Dawson <37661442+LightenedOne@users.noreply.github.com>
2020-02-16 10:43:04 -0800

    Basic text manipulation set up.

---
+++ Text101/Assets/Resources/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class  AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI storyText;
+    [SerializeField] TextMeshProUGUI titleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleText.text = "No longer placeholder.";
+        storyText.text = "I have been edited programmatically. Easy mode.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
