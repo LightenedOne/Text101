c54b0971a2e4b484556ff04b5298d5d4350bbffa
Dawson <37661442+LightenedOne@users.noreply.github.com>
2020-02-22 19:04:40 -0800

    Completed Tutorial

--- Text101/Assets/Resources/Scripts/AdventureGame.cs
+++ Text101/Assets/Resources/Scripts/AdventureGame.cs
@@ -1,25 +1,43 @@
 ﻿using UnityEngine;
 using TMPro;
+using System;
 
-public class  AdventureGame : MonoBehaviour
+public class AdventureGame : MonoBehaviour
 {
+    #region Serialized Variables
     [SerializeField] TextMeshProUGUI storyText;
     [SerializeField] TextMeshProUGUI titleText;
     [SerializeField] State startingState;
+    [SerializeField] string titleString;
+    #endregion
 
     State state;
+
 
     // Start is called before the first frame update
     void Start()
     {
         state = startingState;
-        titleText.text = ("No longer placeholder.");
+        titleText.text = titleString;
         storyText.text = state.GetStateStory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+            storyText.text = state.GetStateStory();
+        }
     }
 }
